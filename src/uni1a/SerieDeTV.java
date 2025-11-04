e69585a0ab2a812c0e4f6058999ff61b67b11e9a
Joniel Ortiz <cortizq2@est.ups.edu.ec>
2025-11-03 21:55:06 -0500

    Implementacion Clase Temporada

--- src/uni1a/SerieDeTV.java
+++ src/uni1a/SerieDeTV.java
@@ -3,13 +3,17 @@
  */
 package uni1a;
 
+import java.util.ArrayList;
+
 // Subclase SerieDeTV que extiende de ContenidoAudiovisual
 public class SerieDeTV extends ContenidoAudiovisual {
     private int temporadas;
+    private ArrayList<Temporada> listaTemporadas;
 
     public SerieDeTV(String titulo, int duracionEnMinutos, String genero, int temporadas) {
         super(titulo, duracionEnMinutos, genero);
         this.temporadas = temporadas;
+        this.listaTemporadas = new ArrayList<>();
     }
 
     public int getTemporadas() {
@@ -19,15 +23,22 @@ public class SerieDeTV extends ContenidoAudiovisual {
     public void setTemporadas(int temporadas) {
         this.temporadas = temporadas;
     }
-    
+
+    public void agregarTemporada(int numero) {
+        listaTemporadas.add(new Temporada(numero));
+
+    }
+
     @Override
     public void mostrarDetalles() {
-        System.out.println("Detalles de la película:");
+        int totalTemporadas = temporadas + listaTemporadas.size();
+        System.out.println("");
+        System.out.println("Detalles de la Serie:");
         System.out.println("ID: " + getId());
         System.out.println("Título: " + getTitulo());
         System.out.println("Duración en minutos: " + getDuracionEnMinutos());
         System.out.println("Género: " + getGenero());
-        System.out.println("Temporadas: " + this.temporadas);
-        System.out.println();
+        System.out.println("Temporadas: " + totalTemporadas);
+        System.out.println("");
     }
 }
---
+++ src/uni1a/Temporada.java
@@ -0,0 +1,17 @@
+package uni1a;
+
+public class Temporada {
+    private int numero;
+
+    public Temporada(int numero) {
+        this.numero = numero;
+    }
+
+    public int getnumero() {
+        return numero;
+    }
+    public void setnumero(int numero) {
+        this.numero = numero;
+    }
+
+}
